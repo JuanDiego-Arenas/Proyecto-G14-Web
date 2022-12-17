d4d5e202046d4d333de3f25f9a2b674b752703eb
SaraGabrielaCC <saragabrielacruz2603@gmail.com>
2022-12-17 11:16:23 -0500

    creacion y configuracion barra de menu

---
+++ SuperCars/front/src/components/Menu/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Nav.css"
+
+function Nav() {
+    return ( 
+        <nav className='nav'>
+            <ul className='container flex'>
+                <li><Link to='/'>Inicio</Link></li>
+                <li><Link to='/Create'>Agregar</Link></li>
+                <li><Link to='/Update'>Modificar</Link></li>
+                <li><Link to='/Delete'>Eliminar</Link></li>
+            </ul>
+        </nav>
+     );
+}
+
+export default Nav;
