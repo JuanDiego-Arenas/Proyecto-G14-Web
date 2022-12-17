5019406035d60efdb88755a3d17f11a45eff2e5f
SaraGabrielaCC <saragabrielacruz2603@gmail.com>
2022-12-17 13:19:47 -0500

    estilos globales

--- SuperCars/back/app.js
+++ SuperCars/back/app.js
@@ -1,6 +1,5 @@
 import express from "express"
 import mongoose from "mongoose";
-import userRouter from "./routes/userRouter.js";
 import carsRouter from "./routes/carsRouter.js";
 import invoiceRouter from "./routes/invoiceRouter.js";
 import tiendaRouter from "./routes/tiendaRouter.js"
@@ -22,18 +21,9 @@ mongoose.connect("mongodb+srv://SuperCars:SuperCars@supercarscluster.ihqmraz.mon
     }
 })
 
-mongoose.connect("mongodb+srv://SuperCars:SuperCars@supercarscluster.ihqmraz.mongodb.net/SuperCarsDB?retryWrites=true&w=majority", (err)=>{
-    if (err) {
-        console.log(err);
-    } else{
-        console.log("Se ha conectado a la base de datos.");
-    }
-})
-
 app.use(cors({origin:"http://localhost:3000"}))
 app.use(express.json())
 
-app.use("/user", userRouter)
 app.use("/cars", carsRouter)
 app.use("/invoice", invoiceRouter)
 app.use("/tienda", tiendaRouter)
